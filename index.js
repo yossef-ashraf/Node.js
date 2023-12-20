dd30697761078bb9eb09e48893d01898096597ee
yossef-ashraf <yossefff2001@gmail.com>
2023-12-20 15:34:22 +0200

    s5

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-const http=require('http');
-
+const express = require('express');
+const app = express()
+const port = 5000 ;
 const hostname= '127.0.0.1';
-const port= '3000';
+const { body , query, validationResult } = require('express-validator');
 
-const server=http.createServer((req,res)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
+
+app.use(express.json())
+
+
+app.get('/api/users', (req, res) => {
+res.send('ur F API Use Node.js - express');
 });
-server.listen(port,hostname,()=>{
+
+app.get('/api/users/:userId', (req, res) => {
+const userId = req.params.userId;
+res.send(userId);
+});
+
+app.post('/api/users/store', (req, res) =>{
+const body = req.body;
+res.send(body);
+})
+
+app.post('/api/validator',[body('name').notEmpty().isLength({min:5})], (req, res) =>{
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      return res.send(`Hello, ${req.body.person}!`);
+    }else{
+        return res.send(result);
+    }
+
+    const body = req.body;
+    res.send(body);
+})
+
+app.get('/hello', query('person').notEmpty(), (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      return res.send(`Hello, ${req.query.person}!`);
+    }
+  
+    res.send({ errors: result.array() });
+});
+
+
+
+// listen is function to run server
+app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
